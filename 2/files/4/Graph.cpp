08d65263228f5ab70692869833fc2218cec59e13
ZePedroResende <j.p.resende68@gmail.com>
2018-05-02 21:08:18 +0100

    git ignore updated

---
+++ 2/files/4/Graph.cpp
@@ -0,0 +1,17 @@
+#include "Graph.h"
+
+vector<string> Graph::getAuthors(){
+
+}
+
+void Graph::addColaborator(string pessoa){
+  colaboracao[]
+}
+
+void Graph::printColaborator(string pessoa){
+
+}
+
+void Graph::printRede(){
+
+}
