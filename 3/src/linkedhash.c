8956f00a42361d94f7b9935e03714ad753153ce2
tiago <tiago.tdaa@gmail.com>
2018-06-08 15:59:07 +0100

    Add dictionary and its parser

--- 3/src/linkedhash.c
+++ 3/src/linkedhash.c
@@ -27,19 +27,19 @@ struct nlist *install(char *name, char *defn)
     unsigned hashval;
     if ((np = lookup(name)) == NULL) { /* not found */
         np = (struct nlist *) malloc(sizeof(*np));
-        if (np == NULL || (np->name = strdup(name)) == NULL)
+        if (np == NULL || (np->name = strdupl(name)) == NULL)
           return NULL;
         hashval = hash(name);
         np->next = hashtab[hashval];
         hashtab[hashval] = np;
     } else /* already there */
         free((void *) np->defn); /*free previous defn */
-    if ((np->defn = strdup(defn)) == NULL)
+    if ((np->defn = strdupl(defn)) == NULL)
        return NULL;
     return np;
 }
 
-char *strdup(char *s) /* make a duplicate of s */
+char *strdupl(char *s) /* make a duplicate of s */
 {
     char *p;
     p = (char *) malloc(strlen(s)+1); /* +1 for ’\0’ */
